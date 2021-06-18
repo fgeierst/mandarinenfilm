3ddfded67dcba817e3d3b25bd28dced32c54ffc4
Florian Geierstanger <florian@geierstanger.org>
2021-06-18 15:28:18 +0200

    resolve #3

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <aside id="sidebar">
-    <?php if(is_active_sidebar('primary-widget-area')) { ?>
-        <div id="primary" class="widget-area">
+    <?php if (is_active_sidebar('primary-widget-area')) { ?>
+        <div id="footer-widgets" class="widget-area footer-widget-area">
             <ul class="xoxo">
                 <?php dynamic_sidebar('primary-widget-area'); ?>
             </ul>
